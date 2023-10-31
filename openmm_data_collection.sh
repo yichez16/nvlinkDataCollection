79759182c9fb09a25a220e631d0489313f853c98
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-10-30 22:10:35 -0700

    Update openmm_data_collection.sh

--- openmm_data_collection.sh
+++ openmm_data_collection.sh
@@ -9,7 +9,7 @@ sudo ./CUPTI_receiver 0 1 0 > rf.csv &
 # Wait for 5 seconds
 sleep 5
 
-for i in $(seq 1 2);
+for i in $(seq 1 5);
 do
     # Run the second command in the foreground (concurrently with the first command)
     python benchmark.py --platform CUDA --test rf --device 0,1 
