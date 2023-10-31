0f4b9fde8d4305c58e5eae0c5312d1624755e74a
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-10-30 21:58:19 -0700

    Update openmm_data_collection.sh

--- openmm_data_collection.sh
+++ openmm_data_collection.sh
@@ -10,11 +10,11 @@ sudo ./CUPTI_receiver 0 1 0 > rf.csv &
 sleep 2
 
 # Launch program B ten times with a 5-second gap in between
-# for i in {1..5}
-# do
-#     # python benchmark.py --platform CUDA --test rf --device 0,1 &
-#     sleep 2
-# done
+for i in {1..1}
+do
+    python benchmark.py --platform CUDA --test rf --device 0,1 &
+    sleep 2
+done
 
 # Find and kill program A
 sudo pkill -f "./CUPTI_receiver"
@@ -25,160 +25,5 @@ sleep 2
 ##########################################################
 
 
-# # Start program A in the background
-# sudo ./CUPTI_receiver 0 1 0 > pme.csv &
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# # Launch program B ten times with a 5-second gap in between
-# for i in {1..10}
-# do
-#     python benchmark.py --platform CUDA --test pme --device 0,1 &
-#     sleep 5
-# done
-
-# # Wait for all instances of program B to finish
-# wait
-
-# # Find and kill program A
-# pkill ./CUPTI_receiver 0 1 0 > pme.csv
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# ##########################################################
-
-# # Start program A in the background
-# sudo ./CUPTI_receiver 0 1 0 > apoa1rf.csv &
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# # Launch program B ten times with a 5-second gap in between
-# for i in {1..10}
-# do
-#     python benchmark.py --platform CUDA --test apoa1rf --device 0,1 &
-#     sleep 5
-# done
-
-# # Wait for all instances of program B to finish
-# wait
-
-# # Find and kill program A
-# pkill ./CUPTI_receiver 0 1 0 > apoa1rf.csv
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# ##########################################################
-
-# # Start program A in the background
-# sudo ./CUPTI_receiver 0 1 0 > apoa1pme.csv &
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# # Launch program B ten times with a 5-second gap in between
-# for i in {1..10}
-# do
-#     python benchmark.py --platform CUDA --test apoa1pme --device 0,1 &
-#     sleep 5
-# done
-
-# # Wait for all instances of program B to finish
-# wait
-
-# # Find and kill program A
-# pkill ./CUPTI_receiver 0 1 0 > apoa1pme.csv
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# ##########################################################
-
-# # Start program A in the background
-# sudo ./CUPTI_receiver 0 1 0 > apoa1ljpme.csv &
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# # Launch program B ten times with a 5-second gap in between
-# for i in {1..10}
-# do
-#     python benchmark.py --platform CUDA --test apoa1ljpme --device 0,1 &
-#     sleep 5
-# done
-
-# # Wait for all instances of program B to finish
-# wait
-
-# # Find and kill program A
-# pkill ./CUPTI_receiver 0 1 0 > apoa1ljpme.csv
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# ##########################################################
-
-# # Start program A in the background
-# sudo ./CUPTI_receiver 0 1 0 > amber20-dhfr.csv &
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# # Launch program B ten times with a 5-second gap in between
-# for i in {1..10}
-# do
-#     python benchmark.py --platform CUDA --test amber20-dhfr --device 0,1 &
-#     sleep 5
-# done
-
-# # Wait for all instances of program B to finish
-# wait
-
-# # Find and kill program A
-# pkill ./CUPTI_receiver 0 1 0 > amber20-dhfr.csv
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# ##########################################################
-
-# # Start program A in the background
-# sudo ./CUPTI_receiver 0 1 0 > amber20-cellulose.csv &
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# # Launch program B ten times with a 5-second gap in between
-# for i in {1..10}
-# do
-#     python benchmark.py --platform CUDA --test amber20-cellulose --device 0,1 &
-#     sleep 5
-# done
-
-# # Wait for all instances of program B to finish
-# wait
-
-# # Find and kill program A
-# pkill ./CUPTI_receiver 0 1 0 > amber20-cellulose.csv
-
-
-# # Wait for 5 seconds
-# sleep 5
-
-# ##########################################################
 
 
