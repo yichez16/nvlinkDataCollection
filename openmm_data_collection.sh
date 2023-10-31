54d4b7eb1aef1b2e4d6214c753e344a5075a9b6a
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-10-30 21:28:52 -0700

    Update openmm_data_collection.sh

--- openmm_data_collection.sh
+++ openmm_data_collection.sh
@@ -4,7 +4,7 @@
 # rf,pme,apoa1rf,apoa1pme,apoa1ljpme,amber20-dhfr,amber20-cellulose
 
 # Start program A in the background
-./CUPTI_receiver 0 1 0 > rf.csv &
+sudo ./CUPTI_receiver 0 1 0 > rf.csv &
 
 
 # Wait for 5 seconds
@@ -13,8 +13,7 @@ sleep 5
 # Launch program B ten times with a 5-second gap in between
 for i in {1..10}
 do
-    python benchmark.py --platform CUDA --test rf --device 0,1  
-    &
+    python benchmark.py --platform CUDA --test rf --device 0,1 &
     sleep 5
 done
 
