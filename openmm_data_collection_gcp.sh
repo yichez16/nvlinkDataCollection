007c3a49c5908a3acbafda0b6a2a526ccc40587e
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-01 07:34:56 -0700

    Update openmm_data_collection_gcp.sh

--- openmm_data_collection_gcp.sh
+++ openmm_data_collection_gcp.sh
@@ -9,7 +9,7 @@ sudo ./CUPTI_receiver 0 1 0 >> rf.csv &
 # Wait for 5 seconds
 sleep 5
 
-for i in $(seq 1 10);
+for i in $(seq 1 40);
 do
     echo "Launching benchmark for the $i time"
     python benchmark.py --platform CUDA --test rf --device 0,1  
@@ -34,7 +34,7 @@ sudo ./CUPTI_receiver 0 1 0 >> pme.csv &
 # Wait for 5 seconds
 sleep 5
 
-for i in $(seq 1 30);
+for i in $(seq 1 20);
 do
     echo "Launching benchmark for the $i time"
     python benchmark.py --platform CUDA --test pme --device 0,1  
@@ -58,7 +58,7 @@ sudo ./CUPTI_receiver 0 1 0 >> apoa1rf.csv &
 # Wait for 5 seconds
 sleep 5
 
-for i in $(seq 1 30);
+for i in $(seq 1 20);
 do
     echo "Launching benchmark for the $i time"
     python benchmark.py --platform CUDA --test apoa1rf --device 0,1  
@@ -82,7 +82,7 @@ sudo ./CUPTI_receiver 0 1 0 >> apoa1pme.csv &
 # Wait for 5 seconds
 sleep 5
 
-for i in $(seq 1 30);
+for i in $(seq 1 20);
 do
     echo "Launching benchmark for the $i time"
     python benchmark.py --platform CUDA --test apoa1pme --device 0,1  
@@ -106,7 +106,7 @@ sudo ./CUPTI_receiver 0 1 0 >> apoa1ljpme.csv &
 # Wait for 5 seconds
 sleep 5
 
-for i in $(seq 1 30);
+for i in $(seq 1 20);
 do
     echo "Launching benchmark for the $i time"
     python benchmark.py --platform CUDA --test apoa1ljpme --device 0,1  
@@ -130,7 +130,7 @@ sudo ./CUPTI_receiver 0 1 0 >> amber20-dhfr.csv &
 # Wait for 5 seconds
 sleep 5
 
-for i in $(seq 1 30);
+for i in $(seq 1 20);
 do
     echo "Launching benchmark for the $i time"
     python benchmark.py --platform CUDA --test amber20-dhfr --device 0,1  
@@ -153,7 +153,7 @@ sudo ./CUPTI_receiver 0 1 0 >> amber20-cellulose.csv &
 # Wait for 5 seconds
 sleep 5
 
-for i in $(seq 1 30);
+for i in $(seq 1 33);
 do
     echo "Launching benchmark for the $i time"
     python benchmark.py --platform CUDA --test amber20-cellulose --device 0,1  
