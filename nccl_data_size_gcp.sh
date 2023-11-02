217249a310ef49f50a3a8ddafe4d14ef0ef3cde5
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-01 21:30:26 -0700

    nccl

---
+++ nccl_data_size_gcp.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# Collect nvlink side-channel leakage of 5 nccl operations:
+# allreduce broadcast reduce allGather reduceScatter
+
+# Run the first command in the background
+sudo ./CUPTI_receiver 0 1 0 >> allreduce.csv &
+
+# Sleep for 5 seconds
+sleep 5
+
+for i in $(seq 1 5);
+do
+    # Run the second command in the foreground (concurrently with the first command)
+    echo "Launching op for the $i time"
+    ./nccl_test 0 $i
+
+    # Sleep for 2 seconds
+    sleep 2
+done
+
+
+# Find and kill the first command 
+sudo pkill -f "./CUPTI_receiver"
+
+sleep 5
+
+##########################################################
+
+# Run the first command in the background
+sudo ./CUPTI_receiver 0 1 0 >> broadcast.csv &
+
+# Sleep for 5 seconds
+sleep 5
+
+for i in $(seq 1 5);
+do
+    # Run the second command in the foreground (concurrently with the first command)
+    echo "Launching op for the $i time"
+    ./nccl_test 1 $i
+
+    # Sleep for 2 seconds
+    sleep 2
+done
+
+
+# Find and kill the first command 
+sudo pkill -f "./CUPTI_receiver"
+
+sleep 5
+
+##########################################################
+
+# Run the first command in the background
+sudo ./CUPTI_receiver 0 1 0 >> reduce.csv &
+
+# Sleep for 5 seconds
+sleep 5
+
+for i in $(seq 1 5);
+do
+    # Run the second command in the foreground (concurrently with the first command)
+    echo "Launching op for the $i time"
+    ./nccl_test 2 $i
+    # Sleep for 2 seconds
+    sleep 2
+done
+
+
+# Find and kill the first command 
+sudo pkill -f "./CUPTI_receiver"
+
+sleep 5
+
+##########################################################
+
+# Run the first command in the background
+sudo ./CUPTI_receiver 0 1 0 >> allGather.csv &
+
+# Sleep for 5 seconds
+sleep 5
+
+for i in $(seq 1 5);
+do
+    # Run the second command in the foreground (concurrently with the first command)
+    echo "Launching op for the $i time"
+    ./nccl_test 3 $i
+
+    # Sleep for 2 seconds
+    sleep 2
+done
+
+
+# Find and kill the first command 
+sudo pkill -f "./CUPTI_receiver"
+
+sleep 5
+
+##########################################################
+
+# Run the first command in the background
+sudo ./CUPTI_receiver 0 1 0 >> reduceScatter.csv &
+
+# Sleep for 5 seconds
+sleep 5
+
+for i in $(seq 1 5);
+do
+    # Run the second command in the foreground (concurrently with the first command)
+    echo "Launching op for the $i time"
+    ./nccl_test 4 $i
+
+    # Sleep for 2 seconds
+    sleep 2
+done
+
+
+# Find and kill the first command 
+sudo pkill -f "./CUPTI_receiver"
+
+sleep 5
+
+##########################################################
