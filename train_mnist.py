51f5d2e20da0574cc29f530aa963f4188a87b6a3
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-08 10:52:48 -0800

    Update train_mnist.py

--- train_mnist.py
+++ train_mnist.py
@@ -4,40 +4,57 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-# Define the CNN model with model parallelism
-class Model(nn.Module):
+# Define the CNN model
+class CNN(nn.Module):
     def __init__(self):
-        super(Model, self).__init__()
-        self.layer1 = nn.Sequential(nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2), nn.ReLU())
-        self.layer2 = nn.Sequential(nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2), nn.ReLU())
-        self.layer3 = nn.Sequential(nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2), nn.ReLU())
-        self.layer4 = nn.Sequential(nn.Linear(64 * 7 * 7, 10))
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ).to('cuda:0')  # Send layer1 to GPU 0
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ).to('cuda:1')  # Send layer2 to GPU 1
 
-        # Move layers to respective GPUs
-        self.layer1 = self.layer1.to("cuda:0")
-        self.layer2 = self.layer2.to("cuda:1")
-        self.layer3 = self.layer3.to("cuda:2")
-        self.layer4 = self.layer4.to("cuda:3")
+        self.fc = nn.Linear(7*7*32, 10).to('cuda:1')  # Fully connected layer on GPU 1
 
     def forward(self, x):
-        x = x.to("cuda:0")
-        x = self.layer1(x).to("cuda:0")
-        x = self.layer2(x).to("cuda:1")
-        x = self.layer3(x).to("cuda:2")
-        x = x.view(x.size(0), -1).to("cuda:3")  # Flatten
-        x = self.layer4(x).to("cuda:3")
-        return x
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)  # Flatten
+        out = self.fc(out)
+        return out
     
-class ToyModel(nn.Module):
-    def __init__(self):
-        super(ToyModel, self).__init__()
-        self.net1 = torch.nn.Linear(10, 10).to('cuda:0')
-        self.relu = torch.nn.ReLU()
-        self.net2 = torch.nn.Linear(10, 5).to('cuda:1')
+# class ModelParallelResNet50(ResNet):
+#     def __init__(self, *args, **kwargs):
+#         super(ModelParallelResNet50, self).__init__(
+#             Bottleneck, [3, 4, 6, 3], num_classes=num_classes, *args, **kwargs)
 
-    def forward(self, x):
-        x = self.relu(self.net1(x.to('cuda:0')))
-        return self.net2(x.to('cuda:1'))
+#         self.seq1 = nn.Sequential(
+#             self.conv1,
+#             self.bn1,
+#             self.relu,
+#             self.maxpool,
+
+#             self.layer1,
+#             self.layer2
+#         ).to('cuda:0')
+
+#         self.seq2 = nn.Sequential(
+#             self.layer3,
+#             self.layer4,
+#             self.avgpool,
+#         ).to('cuda:1')
+
+#         self.fc.to('cuda:1')
+
+#     def forward(self, x):
+#         x = self.seq2(self.seq1(x).to('cuda:1'))
+#         return self.fc(x.view(x.size(0), -1))
 
 # Define data transformations and load MNIST dataset
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
@@ -45,7 +62,7 @@ train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, d
 train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
 
 # Initialize the model and optimizer
-model = ToyModel()
+model = CNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
