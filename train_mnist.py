f4df4e1a0635ae2d36e40e53c6cbd8104f44f5bc
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-08 10:59:28 -0800

    Update train_mnist.py

--- train_mnist.py
+++ train_mnist.py
@@ -4,85 +4,72 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-# Define the CNN model
+# Check if two GPUs are available
+if torch.cuda.device_count() < 2:
+    print("Two GPUs are required for this model parallelism example.")
+    exit()
+
+# Define a simple CNN architecture with two layers
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(1, 10, kernel_size=5),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        ).to('cuda:0')  # Send layer1 to GPU 0
-        
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        ).to('cuda:1')  # Send layer2 to GPU 1
+            nn.MaxPool2d(2)
+        ).to('cuda:0') # Assign layer 1 to GPU 0
 
-        self.fc = nn.Linear(7*7*32, 10).to('cuda:1')  # Fully connected layer on GPU 1
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(10, 20, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        ).to('cuda:1') # Assign layer 2 to GPU 1
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = out.reshape(out.size(0), -1)  # Flatten
-        out = self.fc(out)
-        return out
-    
-# class ModelParallelResNet50(ResNet):
-#     def __init__(self, *args, **kwargs):
-#         super(ModelParallelResNet50, self).__init__(
-#             Bottleneck, [3, 4, 6, 3], num_classes=num_classes, *args, **kwargs)
+        x = self.layer1(x)  # First layer on GPU 0
+        x = x.to('cuda:1')  # Transfer intermediate output to GPU 1
+        x = self.layer2(x)  # Second layer on GPU 1
+        return x
 
-#         self.seq1 = nn.Sequential(
-#             self.conv1,
-#             self.bn1,
-#             self.relu,
-#             self.maxpool,
-
-#             self.layer1,
-#             self.layer2
-#         ).to('cuda:0')
-
-#         self.seq2 = nn.Sequential(
-#             self.layer3,
-#             self.layer4,
-#             self.avgpool,
-#         ).to('cuda:1')
-
-#         self.fc.to('cuda:1')
-
-#     def forward(self, x):
-#         x = self.seq2(self.seq1(x).to('cuda:1'))
-#         return self.fc(x.view(x.size(0), -1))
-
-# Define data transformations and load MNIST dataset
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
-train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
-
-# Initialize the model and optimizer
+# Initialize the model
 model = CNN()
+
+# Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
+# Load MNIST dataset
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+
 # Training loop
-for epoch in range(5):
+for epoch in range(10):  # Loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
+        # Get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-        # inputs, labels = inputs.to("cuda:0"), labels
+        inputs, labels = inputs.to('cuda:0'), labels.to('cuda:1')  # inputs on GPU 0, labels on GPU 1
 
+        # Zero the parameter gradients
         optimizer.zero_grad()
 
-        outputs = model(inputs.to("cuda:0"))
+        # Forward pass
+        outputs = model(inputs)
         loss = criterion(outputs, labels)
-        loss.backward()
 
+        # Backward pass and optimize
+        loss.backward()
         optimizer.step()
 
+        # Print statistics
         running_loss += loss.item()
+        if i % 100 == 99:    # Print every 100 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
+            running_loss = 0.0
 
-    print(f"Epoch {epoch+1}, Loss: {running_loss / len(train_loader)}")
-
-print("Training finished.")
+print('Finished Training')
