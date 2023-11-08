361418e5e3773ee4cba64c6f858a7ab80aacad26
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-08 11:20:53 -0800

    Update train_mnist.py

--- train_mnist.py
+++ train_mnist.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn.functional as F
+import time
 
 # Check if GPUs are available
 assert torch.cuda.device_count() >= 4, "This example requires four GPUs"
@@ -55,6 +56,9 @@ def train(model, train_loader, criterion, optimizer, num_epochs):
             
             if batch_idx % 100 == 0:
                 print(f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}")
+
+
+            time.sleep(2)  # Sleep for 1 second after processing each batch
 
 # Start training
 train(model, train_loader, criterion, optimizer, num_epochs=1)
