7c0622ec8b2311185e520615aff5cc4c569b9ca9
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-08 13:50:02 -0800

    Update model_extraction_gcp.sh

--- model_extraction_gcp.sh
+++ model_extraction_gcp.sh
@@ -1,17 +1,24 @@
 
 
 # Start program A in the background
-sudo ./CUPTI_receiver 0 1 0 >> rf.csv &
+sudo ./CUPTI_receiver 0 1 0 >> cnn_layer1.csv &
 
 # Wait for 5 seconds
 sleep 2
 
-sudo ./CUPTI_receiver 2 0 2 >> rf.csv &
+sudo ./CUPTI_receiver 2 0 2 >> cnn_layer2.csv &
 
 # Wait for 5 seconds
 sleep 2
 
-sudo ./CUPTI_receiver 3 2 3 >> rf.csv &
+sudo ./CUPTI_receiver 3 2 3 >> cnn_layer3.csv &
 
 # Wait for 5 seconds
 sleep 2
+
+
+echo "Launching CNN models."
+python train_mnist.py
+sleep 2
+
+sudo pkill -f "./CUPTI_receiver"
