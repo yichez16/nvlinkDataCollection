4a09135ab4399e1fbcdc3a1fb5abf65de655325f
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-08 11:27:26 -0800

    Update train_mnist.py

--- train_mnist.py
+++ train_mnist.py
@@ -54,11 +54,13 @@ def train(model, train_loader, criterion, optimizer, num_epochs):
             loss.backward()
             optimizer.step()
             
-            if batch_idx % 100 == 0:
-                print(f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}")
+            # Print the current iteration number
+            print(f"Iteration {batch_idx + 1}")
+            # if batch_idx % 100 == 0:
+            #     print(f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}")
 
 
-            time.sleep(1)  # Sleep for 1 second after processing each batch
+            # time.sleep(1)  # Sleep for 1 second after processing each batch
 
 # Start training
 train(model, train_loader, criterion, optimizer, num_epochs=1)
