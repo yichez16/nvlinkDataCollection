167306d955a5315a24464ae81633dbb96541774a
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-08 11:03:30 -0800

    Update train_mnist.py

--- train_mnist.py
+++ train_mnist.py
@@ -4,72 +4,52 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-# Check if two GPUs are available
-if torch.cuda.device_count() < 2:
-    print("Two GPUs are required for this model parallelism example.")
-    exit()
-
-# Define a simple CNN architecture with two layers
-class CNN(nn.Module):
+# 1. Define the CNN Model with model parallelism
+class ParallelCNN(nn.Module):
     def __init__(self):
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 10, kernel_size=5),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        ).to('cuda:0') # Assign layer 1 to GPU 0
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(10, 20, kernel_size=5),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        ).to('cuda:1') # Assign layer 2 to GPU 1
-
+        super(ParallelCNN, self).__init__()
+        # Define layer 1 and place it on GPU 0
+        self.layer1 = nn.Conv2d(1, 20, kernel_size=5).to('cuda:0')
+        # Define layer 2 and place it on GPU 1
+        self.layer2 = nn.Conv2d(20, 50, kernel_size=5).to('cuda:1')
+        # Define layer 3 and place it on GPU 2
+        self.fc = nn.Linear(800, 10).to('cuda:2')
+        
     def forward(self, x):
-        x = self.layer1(x)  # First layer on GPU 0
-        x = x.to('cuda:1')  # Transfer intermediate output to GPU 1
-        x = self.layer2(x)  # Second layer on GPU 1
+        # Move input to GPU 0
+        x = x.to('cuda:0')
+        x = self.layer1(x)
+        # Move tensor to next GPU
+        x = x.to('cuda:1')
+        x = self.layer2(x)
+        x = x.view(-1, 800)
+        # Move tensor to next GPU
+        x = x.to('cuda:2')
+        x = self.fc(x)
         return x
 
-# Initialize the model
-model = CNN()
-
-# Define loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01)
-
-# Load MNIST dataset
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-])
-
-train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+# 2. Load the MNIST Dataset
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
 train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
 
-# Training loop
-for epoch in range(10):  # Loop over the dataset multiple times
-    running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
-        # Get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-        inputs, labels = inputs.to('cuda:0'), labels.to('cuda:1')  # inputs on GPU 0, labels on GPU 1
+# 3. Initialize the model and optimizer
+model = ParallelCNN()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
 
-        # Zero the parameter gradients
+# 4. Define the training loop
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # Assume the target is on the CPU, move it to GPU 2 where the last layer is
+        target = target.to('cuda:2')
         optimizer.zero_grad()
-
-        # Forward pass
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-
-        # Backward pass and optimize
+        output = model(data)
+        loss = nn.functional.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
-
-        # Print statistics
-        running_loss += loss.item()
-        if i % 100 == 99:    # Print every 100 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
-            running_loss = 0.0
-
-print('Finished Training')
+        if batch_idx % 10 == 0:
+            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}')
+            
+# 5. Run the training process
+for epoch in range(1, 11):
+    train(model, 'cuda', train_loader, optimizer, epoch)
