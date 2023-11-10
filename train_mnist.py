c1d53a55434dd06dff7d34fe951aa40c3a4818d9
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-10 14:54:28 -0800

    Update train_mnist.py

--- train_mnist.py
+++ train_mnist.py
@@ -12,22 +12,22 @@ assert torch.cuda.device_count() >= 4, "This example requires four GPUs"
 class ModelParallelCNN(nn.Module):
     def __init__(self):
         super(ModelParallelCNN, self).__init__()
-        self.layer1 = nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2).to('cuda:1')
+        self.layer1 = nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2).to('cuda:0')
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.layer2 = nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2).to('cuda:0')
-        self.layer3 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2).to('cuda:2')
+        self.layer2 = nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2).to('cuda:1')
+        self.layer3 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2).to('cuda:3')
         # Corrected the input size to the fully connected layer according to pooling and convolution layers
-        self.fc = nn.Linear(64 * 3 * 3, 10).to('cuda:3') 
+        self.fc = nn.Linear(64 * 3 * 3, 10).to('cuda:2') 
     
     def forward(self, x):
-        x = self.layer1(x.to('cuda:1'))
+        x = self.layer1(x.to('cuda:0'))
         x = self.pool(F.relu(x))
-        x = self.layer2(x.to('cuda:0'))
+        x = self.layer2(x.to('cuda:1'))
         x = self.pool(F.relu(x))
-        x = self.layer3(x.to('cuda:2'))
+        x = self.layer3(x.to('cuda:3'))
         x = self.pool(F.relu(x))
         x = x.view(x.size(0), -1) # Flatten the output
-        x = self.fc(x.to('cuda:3'))
+        x = self.fc(x.to('cuda:2'))
         return x
 
 
@@ -40,7 +40,7 @@ train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=T
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=1024, shuffle=True)
 
 # Loss function and optimizer
-criterion = nn.CrossEntropyLoss().to('cuda:3') # The loss function needs to be on the same GPU as the last layer
+criterion = nn.CrossEntropyLoss().to('cuda:2') # The loss function needs to be on the same GPU as the last layer
 optimizer = torch.optim.Adam(model.parameters())
 
 # Training loop
@@ -55,8 +55,8 @@ def train(model, train_loader, criterion, optimizer, num_iterations):
                 return
             
             optimizer.zero_grad()
-            output = model(data.to('cuda:1'))
-            loss = criterion(output, target.to('cuda:3'))
+            output = model(data.to('cuda:0'))
+            loss = criterion(output, target.to('cuda:2'))
             loss.backward()
             optimizer.step()
             current_iteration += 1
