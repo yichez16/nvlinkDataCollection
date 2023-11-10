120a66d364de9706f0dd264cced30ef50805fa35
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-10 15:25:20 -0800

    Update CUPTI_receiver.cu

--- CUPTI_receiver.cu
+++ CUPTI_receiver.cu
@@ -81,7 +81,7 @@ int main(int argc, char **argv) {
 	//"nvlink_receive_throughput",
 	"nvlink_total_data_received",// works
 	//"pcie_total_data_received",
-	//"nvlink_total_data_transmitted",// works
+	"nvlink_total_data_transmitted",// works
 	//  "nvlink_total_nratom_data_transmitted" , // works
 	// "nvlink_total_ratom_data_transmitted" ,
 	//  "nvlink_total_response_data_received" ,// works
