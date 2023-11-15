c429e7b00ab7cc33a8ae033852f61598d5006091
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-15 14:14:03 -0800

    Create ml_layer_gcp copy.sh

---
+++ ml_layer_gcp copy.sh
@@ -0,0 +1,63 @@
+# Start profiler in the background
+sudo ./CUPTI_receiver 1 0 1 pcie_total_data_received > single_iteration_mlp_layer1.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+# Start profiler in the background
+sudo ./CUPTI_receiver 3 1 3 pcie_total_data_received > single_iteration_mlp_layer2.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+
+# Start profiler in the background
+sudo ./CUPTI_receiver 2 3 2 pcie_total_data_received > single_iteration_mlp_layer3.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
