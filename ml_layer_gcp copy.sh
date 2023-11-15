4c2d50a5cc4c2451f933fd27217f68dd6e629cce
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-15 14:14:20 -0800

    Update ml_layer_gcp copy.sh

--- ml_layer_gcp copy.sh
+++ ml_layer_gcp copy.sh
@@ -1,5 +1,5 @@
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 1 pcie_total_data_received > single_iteration_mlp_layer1.csv &
+sudo ./CUPTI_receiver 1 0 1 nvlink_total_data_received > single_iteration_mlp_layer1.csv &
 
 # Wait for 2 seconds
 sleep 2
@@ -20,7 +20,7 @@ sleep 2
 ################################################################################################
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 3 1 3 pcie_total_data_received > single_iteration_mlp_layer2.csv &
+sudo ./CUPTI_receiver 3 1 3 nvlink_total_data_received > single_iteration_mlp_layer2.csv &
 
 # Wait for 2 seconds
 sleep 2
@@ -42,7 +42,7 @@ sleep 2
 
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 2 3 2 pcie_total_data_received > single_iteration_mlp_layer3.csv &
+sudo ./CUPTI_receiver 2 3 2 nvlink_total_data_received > single_iteration_mlp_layer3.csv &
 
 # Wait for 2 seconds
 sleep 2
