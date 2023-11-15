5eb4e8755b5fb39b61cff5ec7621f4620893d71e
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-15 14:28:39 -0800

    Update model_extraction_gcp.sh

--- model_extraction_gcp.sh
+++ model_extraction_gcp.sh
@@ -6,12 +6,12 @@ sudo ./CUPTI_receiver 0 1 0 > cnn_layer1.csv &
 # Wait for 5 seconds
 sleep 2
 
-sudo ./CUPTI_receiver 2 0 2 > cnn_layer2.csv &
+sudo ./CUPTI_receiver 0 1 2 > cnn_layer2.csv &
 
 # Wait for 5 seconds
 sleep 2
 
-sudo ./CUPTI_receiver 3 2 3 > cnn_layer3.csv &
+sudo ./CUPTI_receiver 0 1 3 > cnn_layer3.csv &
 
 # Wait for 5 seconds
 sleep 2
