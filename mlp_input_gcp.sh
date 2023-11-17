334818b1e7bd8a53ef0006284c158e4c253e68bf
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-17 04:05:39 -0800

    Update mlp_input_gcp.sh

--- mlp_input_gcp.sh
+++ mlp_input_gcp.sh
@@ -19,88 +19,88 @@ sleep 2
 
 ################################################################################################
 
-# # Start profiler in the background
-# sudo ./CUPTI_receiver 0 0 0 pcie_total_data_received > single_iteration_mlp_input_received_256.csv &
+# Start profiler in the background
+sudo ./CUPTI_receiver 0 0 0 pcie_total_data_received > single_iteration_mlp_input_received_256.csv &
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# # Start mlp training
-# echo "Launching mlp."
-# python mlp_train_mnist.py 256
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 256
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# # kill profiler
-# sudo pkill -f "./CUPTI_receiver"
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# ################################################################################################
-
-
-# # Start profiler in the background
-# sudo ./CUPTI_receiver 0 0 0 pcie_total_data_received > single_iteration_mlp_input_received_512.csv &
-
-# # Wait for 2 seconds
-# sleep 2
-
-# # Start mlp training
-# echo "Launching mlp."
-# python mlp_train_mnist.py 512
-
-# # Wait for 2 seconds
-# sleep 2
-
-# # kill profiler
-# sudo pkill -f "./CUPTI_receiver"
-
-# # Wait for 2 seconds
-# sleep 2
-
-# ################################################################################################
-
-# # Start profiler in the background
-# sudo ./CUPTI_receiver 0 0 0 pcie_total_data_received > single_iteration_mlp_input_received_1024.csv &
-
-# # Wait for 2 seconds
-# sleep 2
-
-# # Start mlp training
-# echo "Launching mlp."
-# python mlp_train_mnist.py 1024
-
-# # Wait for 2 seconds
-# sleep 2
-
-# # kill profiler
-# sudo pkill -f "./CUPTI_receiver"
-
-# # Wait for 2 seconds
-# sleep 2
-
-# ################################################################################################
-
-# # Start profiler in the background
-# sudo ./CUPTI_receiver 0 0 0 pcie_total_data_received > single_iteration_mlp_input_received_2048.csv &
-
-# # Wait for 2 seconds
-# sleep 2
-
-# # Start mlp training
-# echo "Launching mlp."
-# python mlp_train_mnist.py 2048
+################################################################################################
 
 
-# # Wait for 2 seconds
-# sleep 2
+# Start profiler in the background
+sudo ./CUPTI_receiver 0 0 0 pcie_total_data_received > single_iteration_mlp_input_received_512.csv &
 
-# # kill profiler
-# sudo pkill -f "./CUPTI_receiver"
+# Wait for 2 seconds
+sleep 2
 
-# # Wait for 2 seconds
-# sleep 2
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 512
 
-# ################################################################################################
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+# Start profiler in the background
+sudo ./CUPTI_receiver 0 0 0 pcie_total_data_received > single_iteration_mlp_input_received_1024.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+# Start profiler in the background
+sudo ./CUPTI_receiver 0 0 0 pcie_total_data_received > single_iteration_mlp_input_received_2048.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 2048
+
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
