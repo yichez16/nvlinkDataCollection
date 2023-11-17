fdf1d245d47d2a018489398052636a4c8130fd3b
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-17 04:22:05 -0800

    Update mlp_train_mnist.py

--- mlp_train_mnist.py
+++ mlp_train_mnist.py
@@ -93,5 +93,5 @@ def train(model, train_loader, criterion, optimizer, num_iterations):
                 return
 
 # Start training for 20 iterations
-train(model, train_loader, criterion, optimizer, num_iterations=20)
+train(model, train_loader, criterion, optimizer, num_iterations=5)
 
