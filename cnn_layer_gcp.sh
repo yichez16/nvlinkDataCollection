36897c0fc1ba3df3600bbadc9c23e71526a7e5cf
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-21 07:44:01 -0800

    layer type

---
+++ cnn_layer_gcp.sh
@@ -0,0 +1,63 @@
+# Start profiler in the background
+sudo ./CUPTI_receiver 1 0 1 l2_write_transactions/l2_read_transactions > mlp_layer1.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+# Start profiler in the background
+sudo ./CUPTI_receiver 1 0 3 nvlink_total_data_received > layer2.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+
+# Start profiler in the background
+sudo ./CUPTI_receiver 1 0 2 nvlink_total_data_received > layer3.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
