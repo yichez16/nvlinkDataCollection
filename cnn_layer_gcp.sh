75821e6ecc0219bc62a70c1be14467a7b29cadc0
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-22 00:08:38 -0800

    cnn layer

--- cnn_layer_gcp.sh
+++ cnn_layer_gcp.sh
@@ -1,18 +1,18 @@
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 1 l2_write_transactions/l2_read_transactions > mlp_layer1.csv &
+sudo ./conv_100 0 > cnn_layer_0.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
-echo "Launching mlp."
-python mlp_train_mnist.py 1024
+echo "Launching."
+python cnn_train_mnist.py 4096
 
 # Wait for 2 seconds
 sleep 2
 
 # kill profiler
-sudo pkill -f "./CUPTI_receiver"
+sudo pkill -f "./conv_100"
 
 # Wait for 2 seconds
 sleep 2
@@ -20,20 +20,62 @@ sleep 2
 ################################################################################################
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 3 nvlink_total_data_received > layer2.csv &
+sudo ./conv_100 1 > cnn_layer_1.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
-echo "Launching mlp."
-python mlp_train_mnist.py 1024
+echo "Launching."
+python cnn_train_mnist.py 4096
 
 # Wait for 2 seconds
 sleep 2
 
 # kill profiler
-sudo pkill -f "./CUPTI_receiver"
+sudo pkill -f "./conv_100"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+# Start profiler in the background
+sudo ./conv_100 2 > cnn_layer_2.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching."
+python cnn_train_mnist.py 4096
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./conv_100"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+# Start profiler in the background
+sudo ./conv_100 3 > cnn_layer_3.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching."
+python cnn_train_mnist.py 4096
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./conv_100"
 
 # Wait for 2 seconds
 sleep 2
@@ -42,22 +84,88 @@ sleep 2
 
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 2 nvlink_total_data_received > layer3.csv &
+sudo ./conv_100 4 > cnn_layer_4.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
-echo "Launching mlp."
-python mlp_train_mnist.py 1024
+echo "Launching."
+python cnn_train_mnist.py 4096
 
 # Wait for 2 seconds
 sleep 2
 
 # kill profiler
-sudo pkill -f "./CUPTI_receiver"
+sudo pkill -f "./conv_100"
 
 # Wait for 2 seconds
 sleep 2
 
 ################################################################################################
+
+# Start profiler in the background
+sudo ./conv_100 5 > cnn_layer_5.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching."
+python cnn_train_mnist.py 4096
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./conv_100"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+
+# Start profiler in the background
+sudo ./conv_100 6 > cnn_layer_6.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching."
+python cnn_train_mnist.py 4096
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./conv_100"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+# Start profiler in the background
+sudo ./conv_100 7 > cnn_layer_7.csv &
+
+# Wait for 2 seconds
+sleep 2
+
+# Start mlp training
+echo "Launching."
+python cnn_train_mnist.py 4096
+
+# Wait for 2 seconds
+sleep 2
+
+# kill profiler
+sudo pkill -f "./conv_100"
+
+# Wait for 2 seconds
+sleep 2
+
+################################################################################################
+
+
