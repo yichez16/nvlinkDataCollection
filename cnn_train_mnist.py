34c98ce2e603ca6dae2db0d875e7537925cd31ed
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-23 00:28:15 -0800

    Update cnn_train_mnist.py

--- cnn_train_mnist.py
+++ cnn_train_mnist.py
@@ -1,130 +1,28 @@
-# import torch
-# import os
-# import sys
-# import tempfile
-# import torch.nn as nn
-# import torchvision
-# import torchvision.transforms as transforms
-# import torch.nn.functional as F
-# import time
-# from torch.nn.parallel import DistributedDataParallel as DDP
-# import torch.distributed as dist
-# import torch.multiprocessing as mp
-# import torch.optim as optim
-# import torch.distributed as dist
-# import torch.multiprocessing as mp
-# from torchvision import datasets, transforms
-# from torch.utils.data import DataLoader
-# import numpy as np
-# import torch
-# from torch.utils.data import Dataset, DataLoader
-
-# class RandomImageNetDataset(Dataset):
-#     def __init__(self, num_samples=4000, num_classes=1000, image_size=(224, 224, 3)):
-#         self.num_samples = num_samples
-#         self.num_classes = num_classes
-#         self.image_size = image_size
-
-#     def __len__(self):
-#         return self.num_samples
-
-#     def __getitem__(self, idx):
-#         # Generate a random image (noise)
-#         image = np.random.rand(*self.image_size).astype(np.float32)
-        
-#         # Normalize the image
-#         image = (image - 0.485) / 0.229
-
-#         # Generate a random label
-#         label = np.random.randint(0, self.num_classes)
-
-#         return torch.tensor(image), torch.tensor(label)
-
-# # Create the dummy datasets
-# train_dataset = RandomImageNetDataset()
-
-
-
-# class ModelParallelCNN(nn.Module):
-#     def __init__(self, dev0, dev1,dev2, dev3, dev4, dev5, dev6, dev7):
-#         super(ModelParallelCNN, self).__init__()
-#         # Define layers
-#         self.layer1 = nn.Conv2d(3, 64, kernel_size=3, padding=1).to(dev0) # input 224, 224 -3+2 +1 = 224
-#         self.layer2 = nn.MaxPool2d(kernel_size=2, stride=2).to(dev1)  # input 26, f = 2, stride = 2, pd = 0, output (224 -2)/2 + 1 = 112
-#         self.layer3 = nn.Conv2d(32, 64, kernel_size=3, padding=1).to(dev2) # input 112,  112-3+2+1 = 112
-#         self.layer4 = nn.MaxPool2d(kernel_size=2, stride=2).to(dev3) # input 112, 56
-#         self.layer5 = nn.Conv2d(64, 128, kernel_size=3, padding=1).to(dev4) # input 56, 56
-#         self.layer6 = nn.MaxPool2d(kernel_size=2, stride=2).to(dev5) # input 56, 28
-#         self.layer7 = nn.Linear(128 * 28 * 28 , 1024).to(dev6) # input 3*3*128,  output 128
-#         self.layer8 = nn.Linear(1024, 1000).to(dev7)# input 64,  output 10
-#         self.relu = nn.ReLU()  # ReLU activation
-
-#     def forward(self, x):
-#         x = self.relu(self.layer1(x.to(dev0)))
-#         x = self.layer2(x.to(dev1))
-#         x = self.relu(self.layer3(x.to(dev2)))
-#         x = self.layer4(x.to(dev3))
-#         x = self.relu(self.layer5(x.to(dev4)))
-#         x = self.layer6(x.to(dev5))
-#         x = x.view(x.size(0), -1)  # Flatten
-#         x = self.relu(self.layer7(x.to(dev6)))
-#         x = self.relu(self.layer8(x.to(dev7)))
-#         return x
-
-
-# # setup(4, 2)
-# # dist.init_process_group("gloo", rank=4, world_size=2)
-# dev0, dev1, dev2, dev3, dev4, dev5, dev6, dev7 = 0,1,3,2,7,6,4,5
-# batch_value = int(sys.argv[1])
-
-# model = ModelParallelCNN(dev0, dev1, dev2, dev3, dev4, dev5, dev6, dev7)
-
-
-# train_loader = DataLoader(dataset=train_dataset, batch_size=batch_value, shuffle=True)
-
-
-# # Loss function and optimizer
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-
-# # Training loop
-# def train(model, train_loader, criterion, optimizer, num_iterations):
-#     model.train()
-#     current_iteration = 0
-#     for epoch in range(100000):  # num_epochs would be defined in your main code
-#         for batch_idx, (data, target) in enumerate(train_loader):
-#             # Stop after 20 iterations
-#             if current_iteration >= num_iterations:
-#                 print("Stopping training.")
-#                 return
-#             # data = data.view(data.size(0), -1) # Flatten the images required for mlp
-#             optimizer.zero_grad()
-#             output = model(data.to(dev0))
-#             loss = criterion(output, target.to(dev7))
-#             loss.backward()
-#             optimizer.step()
-#             current_iteration += 1
-            
-#             print(f"Iteration {current_iteration}: Loss: {loss.item():.6f}")
-            
-#         # Optionally, break here if you want to ensure only 20 iterations irrespective of epochs
-#         if current_iteration >= num_iterations:
-#             print("Reached %d iterations. Stopping training." % num_iterations)
-
-#             return
-
-# # Start training for 20 iterations
-# train(model, train_loader, criterion, optimizer, num_iterations=100)
-
-
 import torch
+import os
+import sys
+import tempfile
 import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import time
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+import torch.multiprocessing as mp
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
 
-# Step 2: Define the Dummy Dataset Class
-class RandomDataset(Dataset):
+
+
+
+class RandomImageNetDataset(Dataset):
     def __init__(self, num_samples=1000, num_classes=1000, image_size=(224, 224, 3)):
         self.num_samples = num_samples
         self.num_classes = num_classes
@@ -144,56 +42,79 @@ class RandomDataset(Dataset):
         label = np.random.randint(0, self.num_classes)
         return torch.tensor(image), torch.tensor(label)
 
-# Step 3: Define the 8-Layer CNN Model
-class CNN(nn.Module):
-    def __init__(self, num_classes=1000):
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1), nn.ReLU())
-        self.layer2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1), nn.ReLU(), nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer3 = nn.Sequential(nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), nn.ReLU())
-        self.layer4 = nn.Sequential(nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1), nn.ReLU(), nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer5 = nn.Sequential(nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.ReLU())
-        self.layer6 = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1), nn.ReLU(), nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc1 = nn.Linear(512 * 28 * 28, 1024)
-        self.fc2 = nn.Linear(1024, num_classes)
-        self.relu = nn.ReLU()
+# Create the dummy datasets
+train_dataset = RandomImageNetDataset()
+
+
+
+class ModelParallelCNN(nn.Module):
+    def __init__(self, dev0, dev1,dev2, dev3, dev4, dev5, dev6, dev7):
+        super(ModelParallelCNN, self).__init__()
+        # Define layers
+        self.layer1 = nn.Conv2d(3, 64, kernel_size=3, padding=1).to(dev0) # input 224, 224 -3+2 +1 = 224
+        self.layer2 = nn.MaxPool2d(kernel_size=2, stride=2).to(dev1)  # input 26, f = 2, stride = 2, pd = 0, output (224 -2)/2 + 1 = 112
+        self.layer3 = nn.Conv2d(32, 64, kernel_size=3, padding=1).to(dev2) # input 112,  112-3+2+1 = 112
+        self.layer4 = nn.MaxPool2d(kernel_size=2, stride=2).to(dev3) # input 112, 56
+        self.layer5 = nn.Conv2d(64, 128, kernel_size=3, padding=1).to(dev4) # input 56, 56
+        self.layer6 = nn.MaxPool2d(kernel_size=2, stride=2).to(dev5) # input 56, 28
+        self.layer7 = nn.Linear(128 * 28 * 28 , 1024).to(dev6) # input 3*3*128,  output 128
+        self.layer8 = nn.Linear(1024, 1000).to(dev7)# input 64,  output 10
+        self.relu = nn.ReLU()  # ReLU activation
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
-        out = self.layer6(out)
-        out = out.view(out.size(0), -1)  # Flatten
-        out = self.fc1(out)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
+        x = self.relu(self.layer1(x.to(dev0)))
+        x = self.layer2(x.to(dev1))
+        x = self.relu(self.layer3(x.to(dev2)))
+        x = self.layer4(x.to(dev3))
+        x = self.relu(self.layer5(x.to(dev4)))
+        x = self.layer6(x.to(dev5))
+        x = x.view(x.size(0), -1)  # Flatten
+        x = self.relu(self.layer7(x.to(dev6)))
+        x = self.relu(self.layer8(x.to(dev7)))
+        return x
 
-# Step 4: Create Dataset and DataLoader
-train_dataset = RandomDataset()
-train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
 
-# Step 5: Initialize Model, Loss Function, and Optimizer
-model = CNN(num_classes=1000)
+# setup(4, 2)
+# dist.init_process_group("gloo", rank=4, world_size=2)
+dev0, dev1, dev2, dev3, dev4, dev5, dev6, dev7 = 0,1,3,2,7,6,4,5
+batch_value = int(sys.argv[1])
+
+model = ModelParallelCNN(dev0, dev1, dev2, dev3, dev4, dev5, dev6, dev7)
+
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_value, shuffle=True)
+
+
+# Loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
-# Step 6: Training Loop
-num_epochs = 10
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+# Training loop
+def train(model, train_loader, criterion, optimizer, num_iterations):
+    model.train()
+    current_iteration = 0
+    for epoch in range(100000):  # num_epochs would be defined in your main code
+        for batch_idx, (data, target) in enumerate(train_loader):
+            # Stop after 20 iterations
+            if current_iteration >= num_iterations:
+                print("Stopping training.")
+                return
+            # data = data.view(data.size(0), -1) # Flatten the images required for mlp
+            optimizer.zero_grad()
+            output = model(data.to(dev0))
+            loss = criterion(output, target.to(dev7))
+            loss.backward()
+            optimizer.step()
+            current_iteration += 1
+            
+            print(f"Iteration {current_iteration}: Loss: {loss.item():.6f}")
+            
+        # Optionally, break here if you want to ensure only 20 iterations irrespective of epochs
+        if current_iteration >= num_iterations:
+            print("Reached %d iterations. Stopping training." % num_iterations)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            return
 
-        if (i + 1) % 100 == 0:
-            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+# Start training for 20 iterations
+train(model, train_loader, criterion, optimizer, num_iterations=100)
 
-print('Training completed')
