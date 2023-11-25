92bb562d0064f92dc1850e247f23870eb1d61dd2
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-25 15:13:09 -0800

    Update transfer_gcp.sh

--- transfer_gcp.sh
+++ transfer_gcp.sh
@@ -27,7 +27,7 @@ sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
@@ -49,7 +49,7 @@ sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
@@ -64,14 +64,14 @@ sleep 2
 
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 1 nvlink_total_data_received > cnn_nvlink_1.csv &
+sudo ./CUPTI_receiver 1 3 1 nvlink_total_data_received > cnn_nvlink_1.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
@@ -85,14 +85,14 @@ sleep 2
 ################################################################################################
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 3 nvlink_total_data_received > cnn_nvlink_2.csv &
+sudo ./CUPTI_receiver 1 3 3 nvlink_total_data_received > cnn_nvlink_2.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
@@ -107,14 +107,14 @@ sleep 2
 
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 2 nvlink_total_data_received > cnn_nvlink_3.csv &
+sudo ./CUPTI_receiver 1 3 2 nvlink_total_data_received > cnn_nvlink_3.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
@@ -128,14 +128,14 @@ sleep 2
 ################################################################################################
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 7 nvlink_total_data_received > cnn_nvlink_4.csv &
+sudo ./CUPTI_receiver 4 6 7 nvlink_total_data_received > cnn_nvlink_4.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
@@ -149,14 +149,14 @@ sleep 2
 ################################################################################################
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 6 nvlink_total_data_received > cnn_nvlink_5.csv &
+sudo ./CUPTI_receiver 4 6 6 nvlink_total_data_received > cnn_nvlink_5.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
@@ -170,14 +170,14 @@ sleep 2
 ################################################################################################
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 4 nvlink_total_data_received > cnn_nvlink_6.csv &
+sudo ./CUPTI_receiver 4 6 4 nvlink_total_data_received > cnn_nvlink_6.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
@@ -191,14 +191,14 @@ sleep 2
 ################################################################################################
 
 # Start profiler in the background
-sudo ./CUPTI_receiver 1 0 5 nvlink_total_data_received > cnn_nvlink_7.csv &
+sudo ./CUPTI_receiver 4 6 5 nvlink_total_data_received > cnn_nvlink_7.csv &
 
 # Wait for 2 seconds
 sleep 2
 
 # Start mlp training
 echo "Launching mlp."
-python mlp_train_mnist.py 8192
+python mlp_train_mnist.py 1024
 
 # Wait for 2 seconds
 sleep 2
