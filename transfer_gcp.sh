da42643826b3fb8a48955868d4409af3a3f14ec4
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-11-25 19:39:23 -0800

    Update transfer_gcp.sh

--- transfer_gcp.sh
+++ transfer_gcp.sh
@@ -1,66 +1,66 @@
-# # Start profiler in the background
-# sudo ./CUPTI_receiver 1 0 1 nvlink_total_data_received > mlp_nvlink_1.csv &
+# Start profiler in the background
+sudo ./CUPTI_receiver 1 1 1 nvlink_total_data_received > mlp_nvlink_1.csv &
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# # Start mlp training
-# echo "Launching mlp."
-# python mlp_train_mnist.py 1024
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# # kill profiler
-# sudo pkill -f "./CUPTI_receiver"
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# ################################################################################################
+################################################################################################
 
-# # Start profiler in the background
-# sudo ./CUPTI_receiver 1 0 3 nvlink_total_data_received > mlp_nvlink_2.csv &
+# Start profiler in the background
+sudo ./CUPTI_receiver 3 3 3 nvlink_total_data_received > mlp_nvlink_2.csv &
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# # Start mlp training
-# echo "Launching mlp."
-# python mlp_train_mnist.py 1024
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# # kill profiler
-# sudo pkill -f "./CUPTI_receiver"
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# ################################################################################################
+################################################################################################
 
 
-# # Start profiler in the background
-# sudo ./CUPTI_receiver 1 0 2 nvlink_total_data_received > mlp_nvlink_3.csv &
+# Start profiler in the background
+sudo ./CUPTI_receiver 2 2 2 nvlink_total_data_received > mlp_nvlink_3.csv &
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# # Start mlp training
-# echo "Launching mlp."
-# python mlp_train_mnist.py 1024
+# Start mlp training
+echo "Launching mlp."
+python mlp_train_mnist.py 1024
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# # kill profiler
-# sudo pkill -f "./CUPTI_receiver"
+# kill profiler
+sudo pkill -f "./CUPTI_receiver"
 
-# # Wait for 2 seconds
-# sleep 2
+# Wait for 2 seconds
+sleep 2
 
-# ################################################################################################
+################################################################################################
 
 
 # Start profiler in the background
