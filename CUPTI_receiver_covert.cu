2c16248336be8f28bb3d8d03a644df7e81fd0aae
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-12-04 21:41:22 -0800

    Update CUPTI_receiver_covert.cu

--- CUPTI_receiver_covert.cu
+++ CUPTI_receiver_covert.cu
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
     int local=0; 
     int remote=1;
     int profile=0;
-    int sizeElement = 64;
+    int sizeElement;
     int *h_local, *h_remote;
     int *d_local, *d_remote;
     
@@ -48,66 +48,66 @@ int main(int argc, char **argv) {
 
     local = atoi(argv[1]);
     remote = atoi(argv[2]);
-    // profile = atoi(argv[3]);
-    // char *ctr_num = argv[4];
-    sizeElement = atoi(argv[3]); // Transfer size is 256 bytes = 1 nvlink packet
+    profile = atoi(argv[3]);
+    char *ctr_num = argv[4];
+    sizeElement = argv[5]; // Transfer size is 256 bytes = 1 nvlink packet
     // sizeElement = 1048576; // Transfer size is 4 MB
     // printf("%d\n", sizeElement);
 
     size_t size = sizeElement * sizeof(int);
 
     // set up profiler
-    // cudaSetDevice(profile);
-    // CUdevice device;
+    cudaSetDevice(profile);
+    CUdevice device;
 
-	// DRIVER_API_CALL(cuInit(0));  
+	DRIVER_API_CALL(cuInit(0));  
 	// Initialize the CUDA driver API Initializes the driver API and must be called before any other function from the driver API in the current process. Currently, the Flags parameter must be 0. If cuInit() has not been called, any function from the driver API will return CUDA_ERROR_NOT_INITIALIZED.
-	// DRIVER_API_CALL(cuDeviceGet(&device, profile));
+	DRIVER_API_CALL(cuDeviceGet(&device, profile));
 	// Returns a handle to a compute device.
 
     // define ctrs to profile
-	// #if PROFILE_ALL_EVENTS_METRICS
-	// const auto event_names = cupti_profiler::available_events(device);
-	// const auto metric_names = cupti_profiler::available_metrics(device);
-	// #else
-	// vector<string> event_names {        
+	#if PROFILE_ALL_EVENTS_METRICS
+	const auto event_names = cupti_profiler::available_events(device);
+	const auto metric_names = cupti_profiler::available_metrics(device);
+	#else
+	vector<string> event_names {        
                
-	// };
-	// vector<string> metric_names {
-    // ctr_num
-	// // "l2_read_transactions",// works
-	// //"nvlink_data_receive_efficiency",
-	// // "nvlink_data_transmission_efficiency",
-	// //"nvlink_overhead_data_received",
-	// //"nvlink_overhead_data_transmitted",
-	// //"nvlink_receive_throughput",
-	// // "nvlink_total_data_received",// works
-	// //"pcie_total_data_received",
-	// // "nvlink_total_data_transmitted",// works
-	// //  "nvlink_total_nratom_data_transmitted" , // works
-	// // "nvlink_total_ratom_data_transmitted" ,
-	// //  "nvlink_total_response_data_received" ,// works
-	// // "nvlink_total_write_data_transmitted",
-	// // "nvlink_transmit_throughput", //works
-	// // "nvlink_user_data_received",
-	// // "nvlink_user_data_transmitted",
-	// // "nvlink_user_nratom_data_transmitted" ,
-	// // "nvlink_user_ratom_data_transmitted",
-	// // "nvlink_user_response_data_received",
-	// // "nvlink_user_write_data_transmitted",
+	};
+	vector<string> metric_names {
+    ctr_num
+	// "l2_read_transactions",// works
+	//"nvlink_data_receive_efficiency",
+	// "nvlink_data_transmission_efficiency",
+	//"nvlink_overhead_data_received",
+	//"nvlink_overhead_data_transmitted",
+	//"nvlink_receive_throughput",
+	// "nvlink_total_data_received",// works
+	//"pcie_total_data_received",
+	// "nvlink_total_data_transmitted",// works
+	//  "nvlink_total_nratom_data_transmitted" , // works
+	// "nvlink_total_ratom_data_transmitted" ,
+	//  "nvlink_total_response_data_received" ,// works
+	// "nvlink_total_write_data_transmitted",
+	// "nvlink_transmit_throughput", //works
+	// "nvlink_user_data_received",
+	// "nvlink_user_data_transmitted",
+	// "nvlink_user_nratom_data_transmitted" ,
+	// "nvlink_user_ratom_data_transmitted",
+	// "nvlink_user_response_data_received",
+	// "nvlink_user_write_data_transmitted",
 
-	// // "l2_write_transactions",  // error
-	// //"dram_read_transactions",
-	// //"dram_write_transactions",
+	// "l2_write_transactions",  // error
+	//"dram_read_transactions",
+	//"dram_write_transactions",
 
 						
-	// };
+	};
 
   
-  	// #endif
-	// CUcontext context;
-	// cuCtxCreate(&context, 0, profile); // context is created on device # profile
-	// //Parameters
+  	#endif
+	CUcontext context;
+	cuCtxCreate(&context, 0, profile); // context is created on device # profile
+	//Parameters
 	// pctx
 	// - Returned context handle of the new context
 	// flags
@@ -144,7 +144,7 @@ int main(int argc, char **argv) {
     cudaMemcpy(d_remote, h_remote, size, cudaMemcpyHostToDevice);
     cudaDeviceSynchronize();
 
-    int blockSize = 64;
+    int blockSize = 1;
     int gridSize = (sizeElement + blockSize - 1) / blockSize;
 
     
@@ -152,28 +152,21 @@ int main(int argc, char **argv) {
     // int blocksPerGrid = (sizeElement + threadsPerBlock - 1) / threadsPerBlock;
     
     // start cupti profiler   
+    cupti_profiler::profiler *p= new cupti_profiler::profiler(event_names, metric_names, context);
+
     for(int j = 0; j < 10000000000; j++){
           
-        // cupti_profiler::profiler *p= new cupti_profiler::profiler(event_names, metric_names, context);
-        // p->start();
+        p->start();
         gettimeofday(&ts,NULL);
         
         test_nvlink <<<gridSize, blockSize>>>(d_remote, d_local, sizeElement); // 56 SMs, 4*32 =  128 threads  (src, det, numElements)  force to transfer data from remote to local.
-        // p->stop();
+        p->stop();
         gettimeofday(&te,NULL);
         // p->print_event_values(std::cout,ts,te);
-        // p->print_metric_values(std::cout,ts,te);
-        // free(p);
-        
-        std::cout   << size
-        // << "," 
-        // << ts.tv_sec*1000000 + ts.tv_usec
-        << "," 
-        << (te.tv_sec - ts.tv_sec) * 1000000 + (te.tv_usec - ts.tv_usec)
-        ;
-        printf("\n");
+        p->print_metric_values(std::cout,ts,te);
 
     }
+    free(p);
 
 
 
