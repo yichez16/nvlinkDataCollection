2061fd4ec041c72aa52d2a149f632289d6a028dc
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-12-04 22:00:47 -0800

    Update CUPTI_receiver_covert.cu

--- CUPTI_receiver_covert.cu
+++ CUPTI_receiver_covert.cu
@@ -164,6 +164,8 @@ int main(int argc, char **argv) {
         gettimeofday(&te,NULL);
         // p->print_event_values(std::cout,ts,te);
         p->print_metric_values(std::cout,ts,te);
+        cudaDeviceSynchronize();
+
 
     }
     free(p);
