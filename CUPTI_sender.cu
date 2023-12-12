eec9debe52ca6680cbc42013fcf339b74c896139
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-12-11 22:14:58 -0800

    Update CUPTI_sender.cu

--- CUPTI_sender.cu
+++ CUPTI_sender.cu
@@ -89,7 +89,8 @@ int main(int argc, char **argv) {
 
     
     int blockSize = 1;
-    int gridSize = 1;
+    int gridSize = (sizeElement + blockSize - 1) / blockSize;
+
 
     std::this_thread::sleep_for(std::chrono::seconds(2));   // wait for synchronization
     
