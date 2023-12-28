05659b7f2c887edc467924f48dfd15d6475781f2
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-12-28 15:06:49 -0800

    Update covert_bandwidth.sh

--- covert_bandwidth.sh
+++ covert_bandwidth.sh
@@ -5,7 +5,7 @@ do
     ./CUPTI_receiver_covert 1 0 1 nvlink_total_data_received 1000 > covert_receiver_$i.csv &
     # Wait 
     sleep 1
-    ./CUPTI_sender 0 1 10000 10 > covert_sender_$i.csv
+    ./CUPTI_sender 0 1 10000 10 
     # kill receiver
     sudo pkill -f "./CUPTI_receiver_covert"
 
