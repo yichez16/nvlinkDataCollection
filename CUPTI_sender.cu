8e767c429d4b59655980b0d2ede4e11aa65a2f8d
Yicheng Zhang <30842016+yichez16@users.noreply.github.com>
2023-12-29 10:55:35 -0800

    Update CUPTI_sender.cu

--- CUPTI_sender.cu
+++ CUPTI_sender.cu
@@ -198,6 +198,8 @@ int main(int argc, char **argv) {
 
         std::this_thread::sleep_for(std::chrono::microseconds(time2sleep)); // Sleep for 1 millisecond (1000 microseconds)
         
+        std::this_thread::sleep_for(std::chrono::microseconds(time2sleep)); // Sleep for 1 millisecond (1000 microseconds)
+
         // gettimeofday(&te2, NULL);  
 
         // std::this_thread::sleep_for(std::chrono::microseconds(time2sleep)); // Sleep for 1 millisecond (1000 microseconds)
